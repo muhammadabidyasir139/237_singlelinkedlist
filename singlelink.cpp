f89552529a0e961a36d124ea390461bd544ac29f
wesclicdev <wesclicdev@gmail.com>
2024-04-25 16:16:05 +0700

    membuat flow untuk mengisi nim dan nama

--- singlelink.cpp
+++ singlelink.cpp
@@ -88,6 +88,14 @@ void traverse() {
         system("pause");
         system("cls");
         return;
+    } else {
+        Node* currentNode = START;
+        while (currentNode != NULL) 
+        {
+            cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+        
     }
 }
 
