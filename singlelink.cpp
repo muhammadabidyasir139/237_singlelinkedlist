581d4ebad0d1cc82bc0a5ee5bd3270e56c1fbc38
wesclicdev <wesclicdev@gmail.com>
2024-04-25 15:45:13 +0700

    membuat perngkondisian jika nim sudah ada

--- singlelink.cpp
+++ singlelink.cpp
@@ -22,6 +22,10 @@ void addNode() {
 
     
 if (START == NULL || nim <= START->noMhs) {
+    if(START != NULL && nim <= START->noMhs) {
+        cout << "NIM sudah ada" << endl;
+        return;
+    }
     nodeBaru->next = START;
     START = nodeBaru;
 }
