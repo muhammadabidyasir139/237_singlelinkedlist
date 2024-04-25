13f4af9800a59a60ff530be1df4c3e5df3be0fad
wesclicdev <wesclicdev@gmail.com>
2024-04-25 15:48:51 +0700

    membuat flow cek nim sudah ada

--- singlelink.cpp
+++ singlelink.cpp
@@ -33,4 +33,13 @@ if (START == NULL || nim <= START->noMhs) {
 Node* previous = START;
 Node* current = START;
 
+while ((current != NULL) && nim >= current->noMhs) {
+    if(nim == current->noMhs) {
+        cout << "NIM sudah ada" << endl;
+        return;
+    }
+    previous = current;
+    current = current->next;
+}
+
 }
